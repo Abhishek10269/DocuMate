abc4356a94afd1d9d666a17aafc9a3aa337e8689
Abhishek kumar <7011042290abhi@gmail.com>
2024-11-07 00:20:11 +0530

    made a dropbox to drop docs

---
+++ documate/app/dashboard/upload/page.tsx
@@ -0,0 +1,13 @@
+import FileUploader from '@/components/FileUploader';
+import React from 'react'
+
+function upload() {
+  return (
+    <div>
+        {/* file uploader */}
+        <FileUploader/>
+    </div>
+  )
+}
+
+export default upload;
