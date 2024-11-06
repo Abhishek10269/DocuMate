e13afa207edcedadbe7dfba9695abbaf725d497e
Abhishek kumar <7011042290abhi@gmail.com>
2024-11-06 14:06:25 +0530

    finished landing page

--- documate/app/page.tsx
+++ documate/app/page.tsx
@@ -2,6 +2,46 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+import { FaGlobeAsia ,FaApple, FaAirbnb,FaEye} from 'react-icons/fa';
+import { BsFillLightningChargeFill } from "react-icons/bs";
+import { TbMessageChatbotFilled } from "react-icons/tb";
+
+
+
+
+const features = [
+  {
+    name:"hello",
+    description:"Kepp all your PDF files securey stored and easily accessible anytime, anywhere",
+    icon:FaGlobeAsia
+  },
+  
+
+  {
+    description:"experience lightning fast answers to your queries, ensuring you get the information you need instantly.",
+    icon:BsFillLightningChargeFill
+  },
+  {
+    description:"Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    icon:TbMessageChatbotFilled
+  },
+  ,
+  {
+    description:"Engage with your PDFs like never before using our intuitive and interactive viewer",
+    icon:FaEye
+  }
+  ,
+  {
+    description:"Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    icon:TbMessageChatbotFilled
+  }
+  ,
+  {
+    description:"Our intelligent chatbot remembers previous interactions, providing a seamless and personalized experience.",
+    icon:TbMessageChatbotFilled
+  }
+];
+
 
 export default function Home() {
   return (
@@ -33,11 +73,28 @@ export default function Home() {
              height={1442}
              className="mb-[-0%] rounded-xl shadow-2xlring-1 ring-gray-900/10"
              />
-             <div aria-hidden="true" className="relative"></div>
+             <div aria-hidden="true" className="relative">
+             <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]"/>
           </div>
         </div>
+        </div>
+
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none
+          lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {features.map(feature => (
+              <div className="relative pl-9">
+                <dt className="inline font-semibold text-gray-900">
+               < feature.icon  aria-hidden="true" className="absolute left-1 top-1 h-5 w-5 text-indigo-600"/>
+                </dt>
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+
+        </div>
       </div>
+
     </main>
   );
-
 }
