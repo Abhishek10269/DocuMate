3d7417b4218ada9ce95ac19350cb28e871e53294
Abhishek kumar <7011042290abhi@gmail.com>
2024-11-07 10:49:24 +0530

    updated eslintrc

--- documate/components/FileUploader.tsx
+++ documate/components/FileUploader.tsx
@@ -6,7 +6,6 @@ import { useDropzone } from 'react-dropzone';
 
 function FileUploader() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
     console.log(acceptedFiles); // Check if files are being accepted
   }, []);
 
@@ -17,7 +16,9 @@ function FileUploader() {
   return (
     <div className='flex flex-col gap-4 items-center max-w-7xl mx-auto'>
       <div
-        {...getRootProps()}
+        {...getRootProps({
+          onDragOver: (e) => e.preventDefault()  // Prevent default drag behavior
+        })}
         className={`p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center justify-center ${
           isFocused || isDragAccept ? 'bg-indigo-300' : 'bg-indigo-100'
         }`}
@@ -26,13 +27,13 @@ function FileUploader() {
         <div className='flex flex-col items-center justify-center'>
           {isDragActive ? (
             <>
-            <RocketIcon className='h-20 w-20 animate-ping'/>
-            <p>Drop the files here ...</p>
+              <RocketIcon className='h-20 w-20 animate-ping'/>
+              <p>Drop the files here ...</p>
             </>
           ) : (
             <>
-            <CircleArrowDown className='h-20 w-20 animate-bounce'/>
-            <p>Drag 'n' drop some files here, or click to select files</p>
+              <CircleArrowDown className='h-20 w-20 animate-bounce'/>
+              <p>Drag 'n' drop some files here, or click to select files</p>
             </>
           )}
         </div>
