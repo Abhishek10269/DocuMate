55e533362fbf5e33379ae8dbdee7062ed5637b55
Abhishek kumar <7011042290abhi@gmail.com>
2024-11-27 23:37:47 +0530

    minor changes

--- documate/hooks/useUpload.ts
+++ documate/hooks/useUpload.ts
@@ -1,18 +1,18 @@
+"use client";
+
 import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { v4 as uuidv4} from "uuid";
 
-const handleUpload = async (file: File) =>{
-    if(!file || !user) return;
-
-    // TODO : FREE/PRO limitation
-
-    const fileIdToUploadTo = uuidv4();
-
-    const storageRef = ref(storage,
-        `users/${user.id}/files/${fileIdToUploadTo}`
-    );
+export enum StatusText {
+    UPLOADING = "Uploading file...",
+    UPLOADED = "File Uploaded Succefully",
+    SAVING = "Saving File to database",
+    GENERATING = "Generating AI Embeddings, This will take a few seconds...",
 }
+
+export type Status = StatusText[keyof StatusText];
 
 function useUpload() {
 
@@ -22,6 +22,19 @@ function useUpload() {
 
     const {user} = useUser();
     const router = useRouter();
+
+    const handleUpload = async (file: File) =>{
+        if(!file || !user) return;
+    
+        // TODO : FREE/PRO limitation
+    
+        const fileIdToUploadTo = uuidv4();
+    
+        const storageRef = ref(
+            storage,
+            `users/${user.id}/files/${fileIdToUploadTo}`
+        );
+    }
 
 }
 
